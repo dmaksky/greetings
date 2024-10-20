f27b6356f3429b593445f2f0bf551517533a331f
Dmitry Akshevsky <dmitry@akshevsky.ru>
2024-10-20 10:18:25 +0300

    Fix tf_testing.sh

--- pipeline/testing/tf_testing.sh
+++ pipeline/testing/tf_testing.sh
@@ -1,5 +1,9 @@
 cd pipeline/testing
 terraform init
+terraform plan \
+  -var folder_id="$YC_FOLDER_ID" \
+  -var service_account_key_file="$YC_ACCOUNT_KEY_FILE" \
+  -var ssh_public_key="$SSH_PUBLIC_KEY"
 terraform apply -auto-approve \
   -var folder_id="$YC_FOLDER_ID" \
   -var service_account_key_file="$YC_ACCOUNT_KEY_FILE" \
